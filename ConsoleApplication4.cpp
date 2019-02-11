acd4cba21669b3c712ccb7302fad2e99bad282c0
25Nwellick <47508462+25Nwellick@users.noreply.github.com>
2019-02-11 18:08:22 +0000

    Add files via upload

--- ConsoleApplication4.cpp
+++ ConsoleApplication4.cpp
@@ -1,35 +1,36 @@
-// ConsoleApplication4.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#include <windows.h>
 #include "pch.h"
 #include <iostream>
 #include <string>
 #include <vector>
+#include <stdio.h>
 
 
 int main()
 {
 	std::cout << "Please create your account " << std::endl;
+
 	std::string accountname;
 	std::string password;
 
 	getline(std::cin, accountname);
 
-	start:
+start:
 	while (password.length() > 10 || password.length() < 5)
 	{
 		std::cout << "The password must be between 5 and 10 characters.Include an UpperLetter" << std::endl;
-            std::cin >> password;
+		std::cin >> password;
 	}
 
 	bool foundUpperLetter = false;
-	for (int x = 0; x < password.length(); x++)
+	for (int x = 0; x <= password.length(); x++)
 	{
 		if (foundUpperLetter == true)
-			
+
 
 			if (password[x] >= 'A' / 65 && password[x] <= 'Z' / 90)
-			foundUpperLetter = true;
+				foundUpperLetter = true;
 
 		std::vector<int> grades;
 		std::vector<std::string> classes{ "Math", "Biology", "Chemistry"," Physics", "Geography", "Macedonian", "English", "French", "Art", "Music", "History", "P.E", "Programming" };
@@ -58,11 +59,28 @@ int main()
 			std::cout << "Damn, you must avoid studying,because you're going to korchagin i do recommend you to start otherwise your education is useless,thank you for your time" << std::endl;
 		break;
 		if (GPA >= 3 && GPA <= 4)
-			std::cout << " Things sometimes happen dude,hopefully you can get to a 4.5,i recommend" << std::endl;
-			
+			std::cout << " Things sometimes happen dude,hopefully you can get to a 4.5" << std::endl;
+		if (GPA >= 4.5)
+			int a;
+		int b = 0;
+		std::cin >> a;
+		std::string url;
+		std::cout << "Enter url: ";
+		std::cin >> url;
+		std::cout << "how many times do you want to open the url?: ";
+		std::cin >> a;
+		while (b < a)
+		{
+			ShellExecute(NULL, "open", url.c_str(), NULL, NULL, SW_SHOWNORMAL);
+			b++;
+		}
+
+		//tuka sakam da vmetnam ShellExecute ama poradi nekoja pricina mi dava preku 100 errors
 
 
-		
+
+
+
 	}
 	if (!foundUpperLetter)
 	{
